fec774dfa1036a68ed01d4a8c0e024346f0c8ccb
Artyom Zaporozhets <kraysent@gmail.com>
2021-07-08 15:46:20 +0300

    fits reader improved

--- fits_reader.py
+++ fits_reader.py
@@ -2,8 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
-filename = 'uk/MAP2021-04-29T10-45-17.422.fits'
+def plot_picture(filename: str):
+    data = fits.getdata(filename)
+    plt.imshow(data)
+    plt.show()
 
-data = fits.getdata(filename)
+def plot_difference(filename1: str, filename2: str):
+    data1 = fits.getdata(filename1)
+    data2 = fits.getdata(filename2)
+    diff = data2 - data1
+    plt.imshow(diff)
+    plt.show()
 
-plt.imshow(data)
+plot_picture('maps/2021/MAP2021-04-01T00-01-03.016.fits')
+plot_difference('maps/2021/MAP2021-04-01T00-01-03.016.fits', 'maps/2021/MAP2021-04-01T00-03-07.767.fits')
