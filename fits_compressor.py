4f67140c3e6adbaac393d425d62da2480c0a3902
Artyom Zaporozhets <kraysent@gmail.com>
2021-07-10 15:07:11 +0300

    fits compressor fixed

--- fits_compressor.py
+++ fits_compressor.py
@@ -1,6 +1,6 @@
 import numpy as np
 from astropy.io import fits
-from datetime import datetime
+from datetime import date, datetime
 import os
 import pytz
 
@@ -22,35 +22,51 @@ def get_list_of_files(dirpath: str) -> list:
 
     return result
 
-def read_all_files(dirpath: str):
-    filenames = get_list_of_files(dirpath)
-    dates = []
-    arrays = []
+def read_all_files(dirpath: str, span_start: int, span_end: int):
+    filenames = get_list_of_files(dirpath)[span_start:span_end]
+    number_of_files = len(filenames)
+
+    dates = np.zeros((number_of_files), datetime)
+    arrays = np.zeros((number_of_files, 64, 64))
     i = 0
 
     for filename in filenames:
-        i += 1
-
-        if i % 100 == 0:
-            print('{}: {}'.format(i, filename))
         format = 'MAP%Y-%m-%dT%H-%M-%S.%f.fits'
         curr_date = datetime.strptime(filename, format)
 
         curr_filename = os.path.join(dirpath, filename)
         curr_data = read_fits(curr_filename)
-        tz = pytz.timezone('Europe/Moscow')
-        curr_date = curr_date.replace(tzinfo=pytz.utc).astimezone(tz)
 
-        dates.append(curr_date)
-        arrays.append(curr_data)
-        dates, arrays = (list(t) for t in zip(*sorted(zip(dates, arrays))))
+        dates[i] = curr_date
+        arrays[i] = curr_data
 
-    return (
-        np.asarray(dates),
-        np.asarray(arrays)
-    )
+        i += 1
+
+        if i % 100 == 0:
+            print('{}: {}'.format(i, filename))
+
+    perm = dates.argsort()
+
+    return (dates[perm], arrays[perm])
 
 dirpath = 'maps/irmaps/'
-(dates, data) = read_all_files(dirpath)
-result = fits.PrimaryHDU(data)
-result.writeto('result.fits', overwrite=True)
+number_of_files = len(get_list_of_files(dirpath))
+N = 10
+step = int(number_of_files / N)
+
+for i in range(N):
+    print(i)
+    (dates, data) = read_all_files(dirpath, i * step, (i + 1) * step)
+
+    result = fits.PrimaryHDU(data)
+    result.header['DATES'] = 'example'
+    format = '%d-%m-%Y-%H-%M-%S.%f'
+    first = True
+    for d in dates:
+        if not first:
+            result.header['DATES'] += ','
+
+        result.header['DATES'] += d.strftime(format)
+        first = False
+
+    result.writeto('result{}.fits'.format(i), overwrite=True)
