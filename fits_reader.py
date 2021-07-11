4b78a8a4c8c4f39a6eef223eb33b58afb8843b7b
Artyom Zaporozhets <kraysent@gmail.com>
2021-07-11 22:47:43 +0300

    fits_reader now can save and read temperatures and dates to pickle

--- fits_reader.py
+++ fits_reader.py
@@ -1,11 +1,15 @@
 from divisor import divide_cube
 from typing import Tuple
-from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+from pandas import DataFrame
 
 from fits_worker import FITSWorker, get_list_of_files
 from read_pickle import get_field_from_pickle
+
+def dump_data(data: dict, output_filename: str):
+    res_df = DataFrame.from_dict(data)
+    res_df.to_pickle(output_filename)
 
 def compress_fits():
     dirpath = 'maps/irmaps/'
@@ -13,12 +17,9 @@ def compress_fits():
 
     FITSWorker.compress(dirpath, N)
 
-def process_data(data: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-    splitted_data = divide_cube(data, 8, 8)
+def process_data(data: np.ndarray, ncols: int, nrows: int) -> Tuple[np.ndarray, np.ndarray]:
+    splitted_data = divide_cube(data, ncols, nrows)
     return splitted_data.mean(axis = (3, 4)) / 10
-
-def is_night(dt: datetime) -> bool:
-    return dt.hour > 0 and dt.hour < 5
 
 def run1():
     filenames = get_list_of_files('maps/', print_full_paths = True)[:3]
@@ -41,7 +42,10 @@ def run2():
     (dates1, data) = FITSWorker.concat_list_of_fits(filenames)
     # (data, _) = FITSWorker.read_fits('maps/misc/resultold.fits')
     # dates1 = FITSWorker.get_list_of_dates('maps/2021/')
-    temps1 = process_data(data)
+
+    ncols = 8
+    nrows = 8
+    temps1 = process_data(data, ncols, nrows)
 
     (dates2, temps2) = get_field_from_pickle('temperature/all_data.pkl', 'TEMP')
     min_temps = np.zeros((len(dates1)))
@@ -51,16 +55,25 @@ def run2():
         dates_diff = np.abs(dates1[i] - dates2)
         min_temps[i] = temps2[np.argmin(dates_diff)]
 
-    ncols = 8
-    nrows = 8
-    _, axes = plt.subplots(ncols, nrows)
+    dump = {}
+    dump['DATE'] = dates1
+    dump['TEMP'] = min_temps
 
     for i in range(ncols):
         for j in range(nrows):
-            axes[i, j].plot(temps1[:, i, j], min_temps[:], 'ro', markersize = 0.05)
-            axes[i, j].set_xlim(-40, 15)
-            axes[i, j].set_ylim(-30, 15)
+            dump['TEMP_SKY_{}_{}'.format(i, j)] = temps1[:, i, j]
+        
+    dump_data(dump, 'pickles/result.pkl')
 
+def run3():
+    filename = 'pickles/result.pkl'
+    (_, dates) = get_field_from_pickle(filename, 'DATE')
+    (_, temps) = get_field_from_pickle(filename, 'TEMP')
+    (_, temps_sky_33) = get_field_from_pickle(filename, 'TEMP_SKY_0_0')
+
+    plt.plot(temps_sky_33, temps, 'ro', markersize = 0.3)
+    plt.xlim(-45, 10)
+    plt.ylim(-25, 15)
     plt.show()
 
-run2()
+run3()
