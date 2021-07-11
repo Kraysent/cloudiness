f03c454454e7eba6fc9d428fd918b99b8a4da872
Artyom Zaporozhets <kraysent@gmail.com>
2021-07-11 11:45:18 +0300

    added function that divides cube into smaller cubes

---
+++ divisor.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def divide_cube(
+    cube: np.ndarray, ncols: int, nrows: int
+) -> np.ndarray:
+    '''
+    * cube: ndarray with shape (number_of_frames, frame_width, frame_height)
+    '''
+    num_of_frames = cube.shape[0]
+    width_step = int(cube.shape[1] / ncols)
+    height_step = int(cube.shape[2] / ncols)
+    res_arr = np.zeros((num_of_frames, ncols, nrows, width_step, height_step))
+
+    for frame in range(num_of_frames):
+        for col in range(ncols):
+            for row in range(nrows):
+                curr_frame = cube[frame]
+                res_arr[frame][col, row] = curr_frame[
+                    col * width_step  : (col + 1) * width_step, 
+                    row * height_step : (row + 1) * height_step
+                ]
+
+    return res_arr
+
+init_array = np.zeros((20, 64, 64))
+res = divide_cube(init_array, 8, 8)
+print(res.shape)
